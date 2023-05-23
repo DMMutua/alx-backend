b35d01c8aacf994ba2e4c90b92e4a02a70fc0cea
DMMutua <lengowriter99@gmail.com>
2023-05-23 16:14:01 +0300

    using the `babel.localeselector` decorator to determine best match for supported languages

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,11 +21,19 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """Checks for Language Preference in App User System and Uses It"""
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 @app.route('/')
 def index():
     """index route to render index template"""
 
-    return render_template('1-index.html')
+    title = gettext('Welcome to Holberton')
+    header = gettext('Hello world')
+    return render_template('2-index.html', title=title, header=header)
 
 
 if __name__ == '__main__':
