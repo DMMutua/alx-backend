a0781ac85fd2a577b2406cde0fc261fc7b500d5c
DMMutua <lengowriter99@gmail.com>
2023-06-21 08:00:21 +0300

    implementing the job processor for managing queued jobs

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,14 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+	console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+//Queuing processes to listen to new jobs
+queue.process('push_notification_code', (job, done) => {
+	const { phoneNumber, message } = job.data;
+	sendNotification(phoneNumber, message);
+	done();
+});
